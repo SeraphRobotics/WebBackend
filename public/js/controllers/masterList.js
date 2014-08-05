1290b51ba14a49fedceda26e02b3cf73a5c75fde
Berkeley Martinez <Berkeley@robotie.com>
2014-08-04 18:46:13 -0700

    refactored masterlist controller $updateOrDelete instance method has been removed. Replaced with $save. Added angular to globals at project root. Will remove globals in individual files as I go along.

---
+++ public/js/controllers/masterList.js
@@ -0,0 +1,84 @@
+'use strict';
+angular.module('masterList', [
+  'lbServices'
+])
+  .controller('masterList', function ($scope, $log, $modal, Part) {
+    function queryParts() {
+      Part.query().$promise
+        .then(function (partsList) {
+          $log.debug(partsList);
+          $scope.parts = partsList;
+        })
+        .catch($log.error)
+      ;
+    }
+    queryParts();
+    $scope.editPart = function (part) {
+      $log.debug('edit Part');
+      $log.debug(part);
+      var modalInstance = $modal.open({
+        templateUrl: 'editPartModal.html',
+        controller: 'editPartModalController',
+        resolve: {
+          part: function () {
+            return part;
+          }
+        }
+      });
+      modalInstance.result.then(function () {
+        queryParts();
+      });
+    };
+    $scope.addNew = function () {
+      $scope.editPart('new');
+    };
+  })
+  .controller('editPartModalController', function ($scope, $log, $modalInstance, part, Part) {
+    function setForMachine() {
+      if ($scope.part.forMachine) {
+        $scope.forMachine = 'Machine';
+      } else {
+        $scope.forMachine = 'Cartridge';
+      }
+    }
+    $scope.partType = function () {
+      $scope.part.forMachine = !$scope.part.forMachine;
+      setForMachine();
+    };
+    var isNew = false;
+    if (part === 'new') {
+      $scope.part = {};
+      $scope.partType();
+      isNew = true;
+    } else {
+      $scope.part = part;
+      setForMachine();
+    }
+    $scope.save = function () {
+      if (isNew) {
+        Part.create({}, $scope.part).$promise.then(function () {
+          isNew = false;
+          $modalInstance.close();
+        }).catch($log.debug);
+      } else {
+        $scope.part.$save()
+          .then(function (success) {
+            $log.debug(success);
+            $modalInstance.dismiss();
+          }).catch($log.debug);
+      }
+    };
+    $scope.deleteThis = function () {
+      if (isNew) {
+        $modalInstance.dismiss();
+      } else {
+        Part.destroyById({id: $scope.part.id}).$promise.then(function () {
+          $modalInstance.close();
+        }).catch($log.debug);
+      }
+    };
+    $scope.cancel = function () {
+      $modalInstance.dismiss();
+    };
+  })
+;
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-/* globals angular, _  */
+/* globals _  */
 'use strict';
 angular.module('app',[
   'services',
@@ -6,6 +6,7 @@ angular.module('app',[
   'customerDetails',
   'customerSupport',
   'machineDetails',
+  'masterList',
   'productGeneration',
   'updateMachine',
   'mm.foundation.modal',
@@ -14,85 +15,6 @@ angular.module('app',[
   'angular-lodash'
 ]).config(function($logProvider){
   $logProvider.debugEnabled(true);
-  })
-.controller('masterList', function ($scope, $log, $modal, Part) {
-    function queryParts() {
-      Part.query().$promise
-        .then(function (partsList) {
-          $log.debug(partsList);
-          $scope.parts = partsList;
-        })
-        .catch($log.error)
-      ;
-    }
-    queryParts();
-    $scope.editPart = function (part) {
-      $log.debug('edit Part');
-      $log.debug(part);
-      var modalInstance = $modal.open({
-        templateUrl: 'editPartModal.html',
-        controller: 'editPartModalController',
-        resolve: {
-          part: function () {
-            return part;
-          }
-        }
-      });
-      modalInstance.result.then(function () {
-        queryParts();
-      });
-    };
-    $scope.addNew = function () {
-      $scope.editPart('new');
-    };
-  })
-  .controller('editPartModalController', function ($scope, $log, $modalInstance, part, Part) {
-    function setForMachine() {
-      if ($scope.part.forMachine) {
-        $scope.forMachine = 'Machine';
-      } else {
-        $scope.forMachine = 'Cartridge';
-      }
-    }
-    $scope.partType = function () {
-      $scope.part.forMachine = !$scope.part.forMachine;
-      setForMachine();
-    };
-    var isNew = false;
-    if (part === 'new') {
-      $scope.part = {};
-      $scope.partType();
-      isNew = true;
-    } else {
-      $scope.part = part;
-      setForMachine();
-    }
-    $scope.save = function () {
-      if (isNew) {
-        Part.create({}, $scope.part).$promise.then(function () {
-          isNew = false;
-          $modalInstance.close();
-        }).catch($log.debug);
-      } else {
-        $scope.part.$updateOrCreate()
-          .then(function (success) {
-            $log.debug(success);
-            $modalInstance.dismiss();
-          }).catch($log.debug);
-      }
-    };
-    $scope.deleteThis = function () {
-      if (isNew) {
-        $modalInstance.dismiss();
-      } else {
-        Part.destroyById({id: $scope.part.id}).$promise.then(function () {
-          $modalInstance.close();
-        }).catch($log.debug);
-      }
-    };
-    $scope.cancel = function () {
-      $modalInstance.dismiss();
-    };
   })
   .controller('compInventory', function ($scope, $log, Part) {
     Part.query().$promise
