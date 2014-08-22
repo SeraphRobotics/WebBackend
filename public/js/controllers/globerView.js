543e5cd85d01b57ac743d4f72d78a043b92befa5
Berkeley Martinez <Berkeley@robotie.com>
2014-08-22 12:39:22 -0700

    Date filter mechanism working, needs more func Functionality not complete Machine Details is working, but machines need more information, such as prev customer and last service

--- public/js/controllers/globerView.js
+++ public/js/controllers/globerView.js
@@ -9,6 +9,7 @@ angular.module('globerView', [
     $scope,
     $filter,
     Machine,
+    Swap,
     Cartridge,
     CartridgeCredit,
     CartridgeReturn,
@@ -18,17 +19,74 @@ angular.module('globerView', [
   )
   {
     $scope.currentDate = $filter('date')(new Date(), 'yyyy-MM-dd');
-    $scope.startDate= $filter('date')(new Date(1987, 2, 15), 'yyyy-MM-dd');
+    $scope.startDate= $filter('date')(new Date(2012, 2, 15), 'yyyy-MM-dd');
     $scope.endDate = $scope.currentDate;
 
+    $scope.printers = {
+      made: 0,
+      shipped: 0,
+      returned: 0
+    };
 
-    $scope.updateData = function(startDate, endDate) {
+    $scope.scanners = {
+      made: 0,
+      shipped: 0,
+      returned: 0
+    };
+
+    $scope.tablets = {
+      made: 0,
+      shipped: 0,
+      returned: 0
+    };
+
+
+    $scope.updateData = function() {
+      $scope.machineMadeData();
+    };
+
+    $scope.machineMadeData = function() {
+      $scope.printers.made = 0;
+      $scope.tablets.made = 0;
+      $scope.scanners.made = 0;
+      _.chain($scope.machines)
+        .filter(function (machine) { //Start Date filter
+          return new Date(machine.dateManufactured) > new Date($scope.startDate);
+        })
+        .filter(function (machine) { //End Date Filter
+          return new Date(machine.dateManufactured) < new Date($scope.endDate);
+        })
+        .forEach(function (machine) {
+          if (machine.machineType === 'printer') {
+            $scope.printers.made += 1;
+          } else if (machine.machineType === 'scanner'){
+            $scope.scanners.made += 1;
+          } else if (machine.machineType === 'tablet') {
+            $scope.tablets.made += 1;
+          } else {
+            $log.debug('Unknown Type', machine);
+          }
+        })
+      ;
+    };
+
+    $scope.filamentData = function() {
+    };
+
+    $scope.cartridgeData = function() {
+    };
+
+    $scope.shipmentData = function() {
+    };
+
+    $scope.swapData = function() {
     };
 
     Machine.query().$promise
       .then(function (machines) {
         $log.debug('Machines', machines);
         $scope.machines = machines;
+        $scope.machineMadeData();
       })
       .catch(function (err) {
         $log.debug(err);
@@ -89,6 +147,16 @@ angular.module('globerView', [
       .then(function (shipments) {
         $scope.shipments = shipments;
         $log.debug('Shipments', shipments);
+      })
+      .catch(function (err) {
+        $log.debug(err);
+      })
+    ;
+
+    Swap.query().$promise
+      .then(function (swaps) {
+        $scope.swaps = swaps;
+        $log.debug('Swaps', swaps);
       })
       .catch(function (err) {
         $log.debug(err);
